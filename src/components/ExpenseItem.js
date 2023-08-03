225ab28395f619d18467bf9853afe6bfc32956a9
willl85 <ngowilfried@gmail.com>
2023-08-03 14:56:43 +0200

    Create public and src folders

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28); // 2 => janv = 0
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount} €</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

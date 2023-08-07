d9a28414902f940211a288e911e7b851956b8c2f
willl85 <ngowilfried@gmail.com>
2023-08-07 20:57:41 +0200

    Add alternative return on JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from './components/Expenses';
 
 function App() {
@@ -22,11 +23,19 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses expenses={expenses}/>
-    </div>
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses expenses={expenses}/>
+  //   </div>
+  // );
+
+  // Alternative return JSX
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Let's get started!"),
+    React.createElement(Expenses, { expenses: expenses })
   );
 }
 

9afdd8a13967da13ecd842f4ad74df8bc987d676
willl85 <ngowilfried@gmail.com>
2023-08-07 21:14:34 +0200

    Alternative Function Syntax

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
 
   return (

fde02f28ad6b64aa773726f84c6a79218d9250ee
willl85 <ngowilfried@gmail.com>
2023-08-10 14:58:43 +0200

    Rendering lists of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,32 +14,22 @@ const Expenses = (props) => {
     setSelectedValue(selectedYear);
   };
 
+
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         onSelectValue={selectedValue}
         onFilteredChange={filteredChangeHandler}
       />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      ></ExpenseItem>
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
